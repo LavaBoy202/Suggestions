f4a8b3b90df484cc841a1c5d30c4f587009c8eae
LavanNithi <lnithian@uwaterloo.ca>
2021-04-27 21:24:09 -0400

    Create words.h

---
+++ words.h
@@ -0,0 +1,10 @@
+// This module provides a collection of words stored in a SSS ADT
+
+// SEASHELL_READONLY
+
+#include "triple-s.h"
+
+// words_create() creates a new SSS that stores a few thousand
+//   English words
+// effects: allocates memory (you must call sss_destroy)
+struct triple_s *words_create(void);
